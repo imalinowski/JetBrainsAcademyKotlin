eff79d6aac1b6a10b8d0f74ab50eea39dec50af2
Ilya Malinowski <malinowski221106@gmail.com>
2021-08-03 00:34:17 +0300

    Prepare for battle > done

--- MineSweeper/minesweeper/task/src/minesweeper/Main.kt
+++ MineSweeper/minesweeper/task/src/minesweeper/Main.kt
@@ -5,37 +5,44 @@ import kotlin.random.Random
 val map = Array(9){CharArray(9){'.'} } // a map
 var mines:Int = 0
 var fake_mines:Int = 0
+var xy = mutableListOf<Pair<Int,Int>>()
 fun main() {
-    print("How many mines do you want on the field? > ")
+    print("How many mines do you want on the field? ")
     placeMines(readLine()!!.toInt().apply { mines = this })
     calcHints()
     printMap()
-    while (mines > 0 && fake_mines > 0){
-        print("Set/delete mines marks (x and y coordinates): > ")
+    while (mines > 0 || fake_mines > 0){
+        print("Set/delete mines marks (x and y coordinates): ")
         val (x,y) = readLine()!!.split(' ').map { it.toInt() - 1}
         if(map[y][x].isDigit())
-            print("There is a number here!")
+            println("There is a number here!")
         else{
             if(map[y][x] == 'X')
-                mines -= 1
+                mines += if (x to y in xy) 1 else -1
             else
-                fake_mines +=1
-            map[y][x] = '*'
+                fake_mines += if (x to y in xy) -1 else 1
+            if (x to y in xy)
+                xy.remove(x to y)
+            else
+                xy.add(x to y)
             printMap()
         }
     }
+    println("Congratulations! You found all the mines!")
 }
 
 fun printMap(){
-    println(" ?123456789?")
-    println("—?—————————?")
+    println(" │123456789│")
+    println("—│—————————│")
     for (i in map.indices) {
-        print("${i + 1}?")
-        for (j in map[i])
-            print(if(j != 'X') j else '.')
-        println("?")
+        print("${i + 1}│")
+        for (j in map[i].indices)
+            if(j to i in xy)
+                print('*')
+            else print(if(map[i][j] != 'X') map[i][j] else '.')
+        println("│")
     }
-    println("—?—————————?")
+    println("—│—————————│")
 }
 
 fun placeMines(n:Int){
