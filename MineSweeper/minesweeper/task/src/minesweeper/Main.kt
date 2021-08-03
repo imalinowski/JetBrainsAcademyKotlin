9d4dafd5a20f9f477c8518196a8641245167e58a
Ilya Malinowski <malinowski221106@gmail.com>
2021-08-03 14:28:46 +0300

    minesweeper battle > in progress <

--- MineSweeper/minesweeper/task/src/minesweeper/Main.kt
+++ MineSweeper/minesweeper/task/src/minesweeper/Main.kt
@@ -3,77 +3,75 @@ package minesweeper
 import kotlin.random.Random
 
 val map = Array(9){CharArray(9){'.'} } // a map
-var mines:Int = 0
-var fake_mines:Int = 0
-var xy = mutableListOf<Pair<Int,Int>>()
+var mines = mutableSetOf<Pair<Int,Int>>()
+var marks = mutableSetOf<Pair<Int,Int>>()
+
 fun main() {
     print("How many mines do you want on the field? ")
-    placeMines(readLine()!!.toInt().apply { mines = this })
-    calcHints()
+    val n = readLine()!!.toInt()
     printMap()
-    while (mines > 0 || fake_mines > 0){
+    while (marks != mines || mines.size == 0){
         print("Set/delete mines marks (x and y coordinates): ")
-        val (x,y) = readLine()!!.split(' ').map { it.toInt() - 1}
-        if(map[y][x].isDigit())
-            println("There is a number here!")
-        else{
-            if(map[y][x] == 'X')
-                mines += if (x to y in xy) 1 else -1
-            else
-                fake_mines += if (x to y in xy) -1 else 1
-            if (x to y in xy)
-                xy.remove(x to y)
-            else
-                xy.add(x to y)
-            printMap()
+        val (x,y,com) = readLine()!!.split(' ')
+        when(com) {
+            "free" -> {
+                if (mines.size == 0) placeMines(n, x.toInt() to y.toInt())
+                if (x.toInt() to y.toInt() in mines){
+
+                }
+                calcHints(x.toInt(),y.toInt())
+            }
+            "mine" -> marks.add(x.toInt() to y.toInt())
+            else -> print("uknown command")
         }
+        printMap()
     }
     println("Congratulations! You found all the mines!")
 }
 
-fun printMap(){
+fun printMap(fail:Boolean = false){
     println(" │123456789│")
     println("—│—————————│")
     for (i in map.indices) {
         print("${i + 1}│")
         for (j in map[i].indices)
-            if(j to i in xy)
-                print('*')
-            else print(if(map[i][j] != 'X') map[i][j] else '.')
+            print(when {
+                    j to i in mines && !fail -> '.'
+                    j to i in marks && !fail -> '*'
+                    else -> map[i][j] })
         println("│")
     }
     println("—│—————————│")
 }
 
-fun placeMines(n:Int){
+fun placeMines(n:Int,notXY : Pair<Int,Int>){
     for (i in 0 until n) {
         var xy: Pair<Int, Int>
         do {
             xy = Random.nextInt(9) to Random.nextInt(9)
-        } while(map[xy.first][xy.second] == 'X')
+        } while(map[xy.first][xy.second] == 'X' && xy == notXY)
         map[xy.first][xy.second] = 'X'
+        mines.add(xy)
     }
 }
 
-fun calcHints(){
-    for (i in map.indices){
-        for (j in map[i].indices){
-            if(map[i][j] == 'X') continue
-            var mines = 0
-            if (j > 0 && map[i][j - 1] == 'X') mines+=1
-            if (map[i][j] == 'X') mines+=1
-            if (j < map.size - 1 && map[i][j+1] == 'X') mines+=1
-            if (i > 0) {
-                if (j > 0 && map[i - 1][j - 1] == 'X') mines+=1
-                if (map[i - 1][j] == 'X') mines+=1
-                if (j < map.size - 1 && map[i - 1][j+1] == 'X') mines+=1
-            }
-            if (i < map.size - 1) {
-                if (j > 0 && map[i + 1][j - 1] == 'X') mines+=1
-                if (map[i + 1][j] == 'X') mines+=1
-                if (j < map.size - 1 && map[i + 1][j+1] == 'X') mines+=1
-            }
-            if(mines > 0) map[i][j] = '0' + mines
-        }
+fun calcHints(x:Int, y:Int) {
+    if (map[y][x] == 'X' || map[y][x] == '/') return
+    var mines = 0
+    for (i in -1..1) // mines count
+        if(y + i in map.indices)
+            for (j in -1..1)
+                if(x + j in map[y + i].indices)
+                    if(map[y + i][x + j] == 'X')
+                        mines += 1
+    if (mines > 0)
+        map[y][x] = '0' + mines
+    else {
+        map[y][x] = '/'
+        for (i in -1..1) // recursion
+            if(y + i in map.indices)
+                for (j in -1..1)
+                    if(x + j in map[y + i].indices)
+                        calcHints(x + j,y + i)
     }
 }
