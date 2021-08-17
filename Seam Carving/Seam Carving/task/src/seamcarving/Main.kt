dbafdf363e8dcd56aeae647e335a15370b6f0c17
Ilya Malinowski <malinowski221106@gmail.com>
2021-08-17 17:09:53 +0300

    first stage completed

--- Seam Carving/Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/Seam Carving/task/src/seamcarving/Main.kt
@@ -1,5 +1,31 @@
 package seamcarving
 
+import java.awt.Color
+import java.awt.Image
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import javax.imageio.ImageIO
+
+
 fun main() {
-    print("Hello, World!")
+    println("Enter rectangle width:")
+    val width = readLine()!!.toInt()
+    println("Enter rectangle height:")
+    val height = readLine()!!.toInt()
+    println("Enter image name:")
+    val name = readLine()!!
+
+    val bImage  = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val g = bImage.graphics
+
+    g.color = Color.BLACK
+    g.drawRect(0,0,width,height)
+
+    g.color = Color.RED
+    g.drawLine(0,0,width - 1,height - 1)
+    g.drawLine(width - 1,0,0,height - 1)
+
+    ImageIO.write(bImage, "png", File("$name"))
 }
